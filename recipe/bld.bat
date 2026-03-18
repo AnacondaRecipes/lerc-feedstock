7eb4befa7ae931766602a2b9cb9080cdf62dc8f7
Andrii Osipov <aosipov@anaconda.com>
2026-03-18 05:20:22 -0700

    PKG-12925: lerc 4.1.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,8 @@ echo on
 cmake -LAH -S .                               ^
    -B build_conda -G "Ninja"                  ^
    -DCMAKE_BUILD_TYPE="Release"               ^
-   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
+   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"  ^
+   -DBUILD_SHARED_LIBS=ON
 if errorlevel 1 exit 1
 
 cmake --build build_conda -- install
--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,7 @@ cmake ${CMAKE_ARGS} -LAH                            \
     -DCMAKE_BUILD_TYPE=Release        \
     -DCMAKE_INSTALL_PREFIX=$PREFIX    \
     -DCMAKE_INSTALL_LIBDIR=lib        \
+    -DBUILD_SHARED_LIBS=ON            \
     ..
 
 make -j${CPU_COUNT}
